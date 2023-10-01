4ca8b8c6bf576521f10a766f11e527d72c27bc06
HasegawaTaizou <caiocoghi@gmail.com>
2023-10-01 11:21:15 -0300

    alo

--- src/assets/js/methods/popUps/execute-edit-accept-action.js
+++ src/assets/js/methods/popUps/execute-edit-accept-action.js
@@ -5,35 +5,36 @@ import fieldsFood from "../../validations/fields/fields-food";
 export default async function executeAcceptAction() {
   this.v$.$touch();
   const isFilledFields = await this.v$.$validate();
-
-  this.validationsPrice();
-  this.validationsIngredients(this.newIngredient, this.ingredientsObject);
-
-  if(isFilledFields && this.validationsIngredients(this.newIngredient, this.ingredientsObject)) {
-  this.ingredientsArray = this.ingredientsObject.map(
-    (ingredient) => ingredient.name
+  const isPriceValid = this.validationsPrice();
+  const isIngredientsValid = this.validationsIngredients(
+    this.newIngredient,
+    this.ingredientsObject
   );
 
-  const foodData = {
-    image: this.downloadURL,
-    price: this.newPrice,
-    title: this.newTitle,
-    ingredients: this.ingredientsArray,
-  };
+  if (isFilledFields && isPriceValid && isIngredientsValid) {
+    this.ingredientsArray = this.ingredientsObject.map(
+      (ingredient) => ingredient.name
+    );
 
-  this.$store.commit("SET_FOOD_DATA", foodData);
+    const foodData = {
+      image: this.downloadURL,
+      price: this.newPrice,
+      title: this.newTitle,
+      ingredients: this.ingredientsArray,
+    };
 
-  this.acceptFunction();
-  this.closePopUp("SET_SHOW_EDIT_FOOD_POP_UP");
-} else {
-  for (const field of fieldsFood) {
-    if (this.v$[field.key].$error) {
-      this.$nextTick(() => {
-        this.$refs[field.ref].focus();
-      });
-      break;
+    this.$store.commit("SET_FOOD_DATA", foodData);
+
+    this.acceptFunction();
+    this.closePopUp("SET_SHOW_EDIT_FOOD_POP_UP");
+  } else {
+    for (const field of fieldsFood) {
+      if (this.v$[field.key].$error) {
+        this.$nextTick(() => {
+          this.$refs[field.ref].focus();
+        });
+        break;
+      }
     }
   }
 }
-}
-  
